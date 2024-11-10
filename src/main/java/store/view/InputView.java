949231d47ae49b9d15f7d1974fb856cf2cf81543
rlaehd62 <rlaehd62@users.noreply.github.com>
2024-11-10 20:45:01 +0900

    feat(view): View 추가 - InputView - OutputView

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,44 @@
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import store.config.constant.Message;
+import store.test.order.Order;
+import store.test.product.Product;
+import store.test.product.SalesProduct;
+
+public class InputView {
+    public static String readOrder() {
+        System.out.println();
+        System.out.println(Message.PURCHASE_ITEM);
+        return Console.readLine();
+    }
+
+    public static String readForMembershipDiscount() {
+        System.out.println();
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
+
+    public static String readForGiveAways(String name) {
+        System.out.println();
+        System.out.printf(Message.MORE_QUANTITY.toString(), name);
+        System.out.println();
+        return Console.readLine();
+    }
+
+    public static String readForPromotion(Order order) {
+        SalesProduct salesProduct = order.getSalesProduct();
+        Product product = salesProduct.getProduct();
+        String name = product.getName();
+        int quantity = order.getAmountOfBuy();
+        System.out.println();
+        System.out.println(String.format("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", name, quantity));
+        return Console.readLine();
+    }
+
+    public static String readForRetry() {
+        System.out.println();
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,96 @@
+package store.view;
+
+import static store.config.constant.Message.CONVENIENCE_INTRO;
+import static store.config.constant.Message.GREETING;
+
+import java.util.List;
+import java.util.Map;
+import store.config.constant.ErrorMessage;
+import store.test.order.Order;
+import store.model.payment.History;
+import store.model.payment.Receipt;
+import store.test.product.Product;
+import store.test.product.SalesProduct;
+import store.test.product.SalesType;
+
+public class OutputView {
+
+    public static void of(String message, boolean isSpaced) {
+        if (isSpaced) {
+            System.out.println();
+        }
+        System.out.println(message);
+    }
+
+    public static void of(ErrorMessage errorMessage, boolean isSpaced) {
+        if (isSpaced) {
+            System.out.println();
+        }
+        System.out.println(errorMessage.toString());
+    }
+
+    public static void of(String message) {
+        System.out.println(message);
+    }
+
+    public static void printInventory(List<Object> objects) {
+        objects.forEach(System.out::println);
+    }
+
+    private static void printHistoryOfPurchase(Receipt receipt) {
+        System.out.println();
+        System.out.println("==============W 편의점================");
+        System.out.println(String.format("%-10s\t%-2s\t%-10s", "상품명", "수량", "금액"));
+
+        Map<String, History> totalHistory = receipt.getTotalHistory();
+        totalHistory.entrySet().forEach(entry -> {
+            History history = entry.getValue();
+            String line = String.format("%-10s\t%-5s\t%,d", history.getName(), history.getQuantity(),
+                    history.getTotalPrice());
+            System.out.println(line);
+        });
+//        receipt.forEach(order -> {
+//            SalesProduct salesProduct = order.getSalesProduct();
+//            Product product = salesProduct.getProduct();
+//            Price price = product.getPrice();
+//            int quantity = order.getTotalQuantity();
+//            String line = String.format("%-10s\t%-5s\t%,d", product.getName(), quantity, price.calculate(quantity));
+//            System.out.println(line);
+//        });
+    }
+
+    private static void printHistoryOfGiveAway(Receipt receipt) {
+        System.out.println("=============증\t\t정===============");
+        List<Order> filteredOrders = receipt.stream()
+                .filter(order -> order.getAmountOfGet() > 0)
+                .toList();
+        filteredOrders.forEach(order -> {
+            SalesProduct salesProduct = order.getSalesProduct();
+            Product product = salesProduct.getProduct();
+            String line = String.format("%-10s\t%-2d", product.getName(), order.getAmountOfGet());
+            System.out.println(line);
+        });
+    }
+
+    private static void printResultOfPurchase(Receipt receipt) {
+        System.out.println("====================================");
+        System.out.println(
+                String.format("%-10s\t%-5d\t%s", "총구매액", receipt.getTotalQuantity(), receipt.getTotalPrice()));
+        System.out.println(String.format("%-10s\t%-5s\t%s", "행사할인", "",
+                receipt.getTotalPolicyDiscount(SalesType.PROMOTIONAL, Order::getAmountOfGet)));
+        System.out.println(String.format("%-10s\t%-5s\t%s", "멤버십할인", "", receipt.getMembershipDiscount()));
+        System.out.println(String.format("%-10s\t%-5s\t%s", "내실돈", "", receipt.getFinalPrice()));
+    }
+
+    public static void printReceipt(Receipt receipt) {
+        printHistoryOfPurchase(receipt);
+        printHistoryOfGiveAway(receipt);
+        printResultOfPurchase(receipt);
+    }
+
+    public static void printGreeting() {
+        System.out.println(GREETING);
+        System.out.println(CONVENIENCE_INTRO);
+    }
+
+}
