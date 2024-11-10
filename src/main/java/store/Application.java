068977441d7d98170e5be851cb49e201b261e59e
rlaehd62 <rlaehd62@users.noreply.github.com>
2024-11-10 20:45:21 +0900

    feat(all): Application 변경

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,19 @@
 package store;
 
+import store.config.SystemBean;
+import store.controller.ControllerBroker;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 시스템 Bean 설정
+        SystemBean bean = SystemBean.getInstance();
+
+        // Controller 중계기 실행
+        ControllerBroker broker = new ControllerBroker();
+        broker.run();
+
+        // 시스템 Bean 초기화
+        bean.clear();
     }
 }
