558ae2eae149a4e924dd53abd73f11504e4003e5
rlaehd62 <rlaehd62@users.noreply.github.com>
2024-11-11 19:16:51 +0900

    refactor(application): 주석 제거

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -6,14 +6,11 @@ import store.controller.ControllerBroker;
 public class Application {
     public static void main(String[] args) {
 
-        // 시스템 Bean 설정
         SystemBean bean = SystemBean.getInstance();
 
-        // Controller 중계기 실행
         ControllerBroker broker = new ControllerBroker();
         broker.run();
 
-        // 시스템 Bean 초기화
         bean.clear();
     }
 }
