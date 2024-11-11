0d2fc8e1ca6d98ff9c99476a0898fb236f7eeb66
rlaehd62 <rlaehd62@users.noreply.github.com>
2024-11-11 19:13:12 +0900

    refactor(utility): FlowHandler 수정 - 원 의도에 맞게 RunTimeException으로 변경 - 재시도 및 Boolean 반환을 위한 함수 추가

--- src/main/java/store/utility/FlowHandler.java
+++ src/main/java/store/utility/FlowHandler.java
@@ -12,7 +12,7 @@ public class FlowHandler {
     public static boolean runWithRetry(Supplier<Boolean> supplier, Consumer<Exception> exceptionHandler) {
         try {
             return supplier.get();
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             exceptionHandler.accept(e);
             return runWithRetry(supplier, exceptionHandler);
         }
@@ -21,8 +21,17 @@ public class FlowHandler {
     public static void run(Runnable runnable, Consumer<Exception> exceptionHandler) {
         try {
             runnable.run();
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             exceptionHandler.accept(e);
+        }
+    }
+
+    public static boolean runWithReturnWithBoolean(Supplier<Boolean> supplier, Consumer<Exception> exceptionHandler) {
+        try {
+            return supplier.get();
+        } catch (RuntimeException e) {
+            exceptionHandler.accept(e);
+            return runWithReturnWithBoolean(supplier, exceptionHandler);
         }
     }
 
@@ -32,23 +41,7 @@ public class FlowHandler {
             if (supplier.get()) {
                 successHandler.run();
             }
-        } catch (Exception e) {
-            exceptionHandler.accept(e);
-            runWithRetry(supplier, exceptionHandler);
-        }
-    }
-
-    public static void runWithRetry(Supplier<Boolean> supplier, Runnable successHandler,
-                                    Runnable failureHandler,
-                                    Consumer<Exception> exceptionHandler) {
-        try {
-            if (supplier.get()) {
-                successHandler.run();
-                return;
-            }
-
-            failureHandler.run();
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             exceptionHandler.accept(e);
             runWithRetry(supplier, exceptionHandler);
         }
